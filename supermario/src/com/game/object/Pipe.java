856889772aa5a0164565ccdcc2a1bc94a3204d1b
THE ANH <THE ANH@LAPTOP-EN8G3PEH>
2024-12-13 20:37:04 +0700

    push lai cho vui

--- supermario/src/com/game/object/Pipe.java
+++ supermario/src/com/game/object/Pipe.java
@@ -1,3 +1,4 @@
+/*
 package com.game.object;
 
 import java.awt.Graphics;
@@ -61,6 +62,75 @@ public class Pipe extends GameObject {
 				(int) getY() + 5,
 				5,
 				(int) getHeight() - 10);
+	}
+	
+	
+	
+}
+*/
+package com.game.object;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import com.game.gfx.Texture;
+import com.game.main.Game;
+import com.game.object.util.ObjectId;
+
+public class Pipe extends GameObject {
+	private Texture tex = Game.getTexture();
+	private int index;
+	private BufferedImage sprite[];
+	
+	public boolean enterable;
+	
+	public Pipe(int x, int y, int width, int height, int index,int scale, boolean enterable) { 
+		super(x, y, ObjectId.Pipe, width, height, scale);
+		this.enterable = enterable;
+		this.index= index;
+		sprite= tex.getPipe2();
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		g.drawImage(sprite[index], (int)getX(), (int)getY(), (int)getWidth(), (int)getHeight(), null);
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		return new Rectangle( (int) getX(), (int) getY(), (int) getWidth(), (int) getHeight());
+	}
+
+	@Override
+	public Rectangle getBoundsTop() {
+		return new Rectangle( (int) (getX() + getWidth()/2 - getWidth()/4),
+				(int) getY(),
+				(int) getWidth()/2,
+				(int) getHeight()/2);
+	}
+
+	@Override
+	public Rectangle getBoundsLeft() {
+		return new Rectangle((int) getX(),
+				(int) (getY()),
+				5,
+				(int) (getHeight() ));
+	}
+
+	@Override
+	public Rectangle getBoundsRight() {
+		return new Rectangle( (int) (getX() + getWidth() -5),
+				(int) getY() ,
+				5,
+				(int) getHeight());
 	}
 	
 	
